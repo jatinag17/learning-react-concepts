612d2416091b1f9dd7342a81dec6f46ef38335fc
jatinag17 <jatinagrawal128@gmail.com>
2024-10-05 19:58:34 +0530

    npm install andpacel, make git ignore file

---
+++ episode-2 igniting our app/app.js
@@ -0,0 +1,23 @@
+// const heading = React.createElement('h1', {id:'heading'}, "Hello World from React");
+// const root = ReactDOM.createRoot(document.querySelector("#root"));
+// root.render(heading);
+// const heading = React.createElement('h1', {id:"heading"}, "Hello World from React");
+//         const root = ReactDOM.createRoot(document.getElementById("root"));
+//         root.render(heading);
+        /* <div id="parent">
+    <div id="child">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+</div> */
+//made the above pattern using react
+import React from "react"
+import ReactDOM from "react-dom"
+const parent = React.createElement("div", { id: "parent" }, 
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
