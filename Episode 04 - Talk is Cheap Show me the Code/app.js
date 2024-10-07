7b5447de8d6e0bda8d48eeddd0c9e8bbe13ff99b
jatinag17 <jatinagrawal128@gmail.com>
2024-10-08 00:07:27 +0530

    make cards using react

--- Episode 04 - Talk is Cheap Show me the Code/app.js
+++ Episode 04 - Talk is Cheap Show me the Code/app.js
@@ -36,11 +36,72 @@ const Header=()=>
     </div>
   );
 };
+// const styleCard={
+//   backgroundColor: "#f0f0f0",
+// };
+// we can also give css in js
+
+const RestaurantCard=()=>{
+  return (
+    <div className="res-card" /* style={styleCard}*/>
+      <img
+        className="res-logo"
+        src="https://b.zmtcdn.com/data/pictures/chains/3/18875863/a7499d72024b4c6f7ef661ecf3cbc8c4_featured_v2.jpg?output-format=webp"
+        alt="res-logo"
+      />
+      <h3>Brijwasi foods</h3>
+      <h4>Sweets , North Indian</h4>
+      <h4>4.5 stars</h4>
+      <h4>30 mins</h4>
+    </div>
+  );
+};
+const Body=()=>{
+  return(
+    <div className="Body">
+    <div className="search">Search</div>
+    <div className="res-container">
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+      <RestaurantCard/>
+    </div>
+    </div>
+  );
+};
+
 const AppLayout=() =>
 {
   return(
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
