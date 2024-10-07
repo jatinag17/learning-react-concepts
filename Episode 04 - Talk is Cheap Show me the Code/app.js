27b96d476cb067ebecd098e9a50473a0f46bb936
jatinag17 <jatinagrawal128@gmail.com>
2024-10-07 10:47:26 +0530

    understanding react layout and make a header

--- Episode 04 - Talk is Cheap Show me the Code/app.js
+++ Episode 04 - Talk is Cheap Show me the Code/app.js
@@ -1,16 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const Title = () => <h1>title tag</h1>;
-
-// const data = api.getData(); // cross site scripting
-
-
-const HeadingComponent = () => (
-  <div id="container">
-    {<Title/>}
-    <h1 className="heading">namaste react Functional component</h1>
-  </div>
-);
+/**
+ * Header
+ *    - Logo
+ *    - Nav Items
+ *  Body
+ *    - Search
+ *    - Restaurant Container
+ *        - Image
+ *        - Name of Restaurant, Rating, Cuisines, Delivery Time
+ *  Footer
+ *    - Copyright
+ *    - Links
+ *    - Address
+ *    - Contact
+ */
+const Header=()=>
+{
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const AppLayout=() =>
+{
+  return(
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<AppLayout />);
