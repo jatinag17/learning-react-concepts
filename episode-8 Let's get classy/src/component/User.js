8d6961240060ce43d72e581a521a9b2dbd05150e
jatinag17 <jatinagrawal128@gmail.com>
2024-11-12 23:55:50 +0530

    make user.js and add into about file

---
+++ episode-8 Let's get classy/src/component/User.js
@@ -0,0 +1,8 @@
+const User=()=>{
+    return <div className="user-card">
+        <h2>Name:Jatin</h2>
+        <h3>Location:Mathura</h3>
+        <h4>contact:XXXXXXXX</h4>
+    </div>
+}
+export default User;
