19df0504277cf54617132aa9cf0f10d223b47096
jatinag17 <jatinagrawal128@gmail.com>
2024-11-13 11:47:13 +0530

    Understanding the use of State Variables in Class based Component

--- episode-8 Let's get classy/src/component/User.js
+++ episode-8 Let's get classy/src/component/User.js
@@ -1,5 +1,11 @@
+import { useState } from "react";
 const User=(props)=>{
+    const [count]=useState(0);
+    // const [sl]=useState(50);
+
     return <div className="user-card">
+        <h1>Count={count}</h1>
+        {/* <h1>ss={sl}</h1> */}
         <h2>Name:{props.name}</h2>
         <h3>Location:Mathura</h3>
         <h4>contact:XXXXXXXX</h4>
--- episode-8 Let's get classy/src/component/UserClass.js
+++ episode-8 Let's get classy/src/component/UserClass.js
@@ -4,15 +4,25 @@ class UserClass extends React.Component{
     constructor(props)
     {
         super(props);
-        console.log(props);
+        this.state={
+            count:0,
+            count2:50,
+        };
+        // console.log(props);
     }
 render()
 {
-    return(<div className="user-card">
-        <h2>Name:{this.props.name}</h2>
+    const{name}=this.props;
+    const{count,count2}=this.state;
+        return(
+      <div className="user-card">
+         <h1>Count: {count}</h1>
+         <h1>Count2: {count2}</h1>
+
+        <h2>Name:{name}</h2>
         <h3>Location:Mathura</h3>
         <h4>contact:XXXXXXXX</h4>
-        </div>
+      </div>
     );
 }
 }
