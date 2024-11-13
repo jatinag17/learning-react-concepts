831cbd43a94d7501bf54223bd42120dd9c3d20dd
jatinag17 <jatinagrawal128@gmail.com>
2024-11-13 09:28:37 +0530

    understanding Class based Component

---
+++ episode-8 Let's get classy/src/component/UserClass.js
@@ -0,0 +1,14 @@
+import React from "react";
+//class based component
+class UserClass extends React.Component{
+render()
+{
+    return(<div className="user-card">
+        <h2>Name:Jatin</h2>
+        <h3>Location:Mathura</h3>
+        <h4>contact:XXXXXXXX</h4>
+        </div>
+    );
+}
+}
+export default UserClass;
