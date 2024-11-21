b5b06d8bcf5465faf6632ef4b531338d985d87c2
jatinag17 <jatinagrawal128@gmail.com>
2024-11-21 19:18:38 +0530

    Adding dynamic login logout button functionlaity using useState hook

--- Episode-6 Exploring the World/src/component/Header.js
+++ Episode-6 Exploring the World/src/component/Header.js
@@ -1,17 +1,26 @@
 import { LOGO_URL } from "../utils/constants";
 import "../../index.css";
+import { useState } from "react";
 const Header = () => {
+  // let btnName="login";
+  const[btnNameReact, setBtnNameReact]=useState("Login"); 
   return (
     <div className="header">
       <div className="logo-container">
         <img className="logo" src={LOGO_URL} />
       </div>
-       <div className="nav-items">
+      <div className="nav-items">
         <ul>
           <li>Home</li>
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="login" onClick={()=>{
+            btnNameReact=="Login" ? setBtnNameReact("Logout"):setBtnNameReact("Login");
+            // console.log(btnNameReact);
+          }}
+            
+            >{btnNameReact}</button>
         </ul>
       </div>
     </div>
